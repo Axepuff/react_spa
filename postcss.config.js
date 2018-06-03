a2259a66d4788bb6f12003e9a4e378c03749017c
MaxAxenov <maksenov@uchi.ru>
2018-06-03 14:38:00 +0300

    fear: react start

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    'postcss-cssnext': {
+      browsers: ['last 2 versions', '> 5%']
+    },
+    'postcss-nested': {}
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+let conf = {
+  entry: ['babel-polyfill', './src/'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: 'dist/'
+  },
+  devServer: {
+    overlay: true
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ],
+  module: {
+    rules: [
+        {
+          test: /\.js$/,
+          use: {
+            loader: 'babel-loader'
+          } 
+        },
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              { loader: 'css-loader', options: { importLoaders: 1 } },
+              'postcss-loader'
+            ]
+          })
+        }
+    ]
+  }
+};
+
+module.exports = (env, options) => {
+  let production = options.mode;
+  conf.devtool = production === 'production' ? 'source-map' : 'eval-sourcemap';
+  return conf;
+}
